9cbcafadf0f227197fce6c8aef2d95bf1d3cd483
Isha Afzaal <25419186+IshaA08@users.noreply.github.com>
2024-02-26 18:02:02 -0700

    Change plot style to ggplot

--- squares.py
+++ squares.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 
 input_values = [1, 2, 3, 4, 5]
 squares = [1, 4, 9, 16, 25]
+plt.style.use('ggplot')
 
 fig, ax = plt.subplots()
 ax.plot(input_values, squares, linewidth=3)
@@ -13,5 +14,6 @@ ax.set_ylabel("Square of Value", fontsize=14)
 
 # Set the size of the tick labels
 ax.tick_params(axis='both', labelsize=14)
+
 
 plt.show()
