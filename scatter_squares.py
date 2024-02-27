f33009ca2b87d9a05a81cedceecf210d200283c9
Isha Afzaal <25419186+IshaA08@users.noreply.github.com>
2024-02-26 18:37:48 -0700

    Use a green colormap on the scatter plot

--- scatter_squares.py
+++ scatter_squares.py
@@ -6,7 +6,12 @@ y_values = [x**2 for x in x_values]
 
 plt.style.use('seaborn-v0_8')
 fig, ax = plt.subplots()
-ax.scatter(x_values, y_values, c='pink', s=10)
+ax.scatter(
+    x_values, 
+    y_values, 
+    c=y_values, 
+    cmap=plt.cm.Greens, 
+    s=10)
 
 # Set the chart title and axes labels
 ax.set_title("Square Numbers", fontsize=24)
