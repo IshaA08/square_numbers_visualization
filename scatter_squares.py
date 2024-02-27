056bb9821782d59019baa49fe230db644dc6cd81
Isha Afzaal <25419186+IshaA08@users.noreply.github.com>
2024-02-26 18:17:20 -0700

    Change x and y value ranges

--- scatter_squares.py
+++ scatter_squares.py
@@ -1,8 +1,12 @@
 import matplotlib.pyplot as plt
 
+# Define the x and y value ranges
+x_values = range(1, 1001)
+y_values = [x**2 for x in x_values]
+
 plt.style.use('seaborn-v0_8')
 fig, ax = plt.subplots()
-ax.scatter(2, 4, s=200)
+ax.scatter(x_values, y_values, c='pink', s=10)
 
 # Set the chart title and axes labels
 ax.set_title("Square Numbers", fontsize=24)
@@ -11,5 +15,8 @@ ax.set_ylabel("Square of Value", fontsize=14)
 
 # Set the size of the tick labels
 ax.tick_params(axis='both', which='major', labelsize=14)
+
+# Set the axes ranges
+ax.axis([0, 1100, 0, 1100000])
 
 plt.show()
