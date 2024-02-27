4b2b83065672e2e54d181a2dc4532a3b83dff36b
Isha Afzaal <25419186+IshaA08@users.noreply.github.com>
2024-02-26 17:53:15 -0700

    Add labels (for the chart title, x- and y-axis, ticks)

--- squares.py
+++ squares.py
@@ -3,6 +3,14 @@ import matplotlib.pyplot as plt
 squares = [1, 4, 9, 16, 25]
 
 fig, ax = plt.subplots()
-ax.plot(squares)
+ax.plot(squares, linewidth=3)
+
+# Set the chart title and label axis
+ax.set_title("Square Numbers", fontsize=24)
+ax.set_xlabel("Value", fontsize=14)
+ax.set_ylabel("Square of Value", fontsize=14)
+
+# Set the size of the tick labels
+ax.tick_params(axis='both', labelsize=14)
 
 plt.show()
